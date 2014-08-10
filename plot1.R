11c1f9f771b55573a42616cd346df8245a15f1b2
Caschr <Casper-Christiansen@live.dk>
2014-08-10 20:44:17 +0200

    Final commit

--- plot1.R
+++ plot1.R
@@ -41,7 +41,7 @@ getdata <- function(dataURL){
 
 }
 makeplot1 <- function() {
-  png(filename="plot1.png", width=481, height=480)
+  png(filename="plot1.png", width=480, height=480)
   par(bg=NA, font.lab=1, ps = 12)
   hist(plotdata$Global_active_power, col = "#FF2424", xlab = "Global Active Power (kilowatts)", main="Global Active Power")
   dev.off()
